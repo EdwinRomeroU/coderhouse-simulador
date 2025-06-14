49087f6a46f609cbc605db3879cace719c42af04
Edwin Romero <jedcaf@gmail.com>
2025-06-14 17:37:10 -0500

    Primera versión funcional con el almacenamiento de una sola letra y sin el ciclo Do While

--- js/main.js
+++ js/main.js
@@ -1,21 +1,61 @@
+// Arreglo de palabras para el simulador
 const palabras = ["javascript", "algoritmos", "desarrollo", "componente", "navegador", "procesador", "aplicacion", "funcion", "variable", "programacion"];
 
+// Palabra seleccionada al azar para que el usuario la adivine
 const palabraSeleccionada = palabras[Math.floor(Math.random() * palabras.length)];
-let usuarioAdivino = false;
+console.log("Palabra seleccionada:", palabraSeleccionada);
 
-do {
+// Arreglo para almacenar las letras adivinadas por el usuario
+const usuarioAdivina = [];
+
+//Arreglo para almacenar las letras que el usuario ingresó y que no están en la palabra
+const usuarioNoAdivina = [];
+
+// Variable para controlar si el usuario ha adivinado la palabra
+let usuarioAdivino = rellenarUsuarioAdivina();
+
+// Función para validar si la letra existe en la palabra seleccionada
+function letraEnPalabra(letra) {
+    return palabraSeleccionada.includes(letra);
+}
+
+// Función para inicializar el arreglo de letras adivinadas
+function rellenarUsuarioAdivina() {
+    // Rellenar el arreglo de letras adivinadas con guiones bajos
+    for (let i = 0; i < palabraSeleccionada.length; i++) {
+        usuarioAdivina[i] = "_";
+    }
+}
+
+// Ciclo principal del simulador
+//do {
     
+    // Prompt para capturar la letra ingresada por el usuario
     let usuarioLetra = prompt("Ingresa una letra:");
 
+    // Validación para asegurarse de que el usuario ingrese una letra y no otro caracter
     let letravalida = /^[a-zA-Z]$/.test(usuarioLetra);
 
+
+
+    // Verifico si la letra es válida
     if (!letravalida) {
-        console.log("Por favor, ingresa una letra válida.");
-        continue;
+        alert("Por favor, ingresa una letra válida.");
+  //      continue;
     } else {
         usuarioLetra = usuarioLetra.toLowerCase();
+
+        if (letraEnPalabra(usuarioLetra)) {
+            // Si la letra está en la palabra, actualizo el arreglo de letras adivinadas
+            for (let i = 0; i < palabraSeleccionada.length; i++) {
+                if (palabraSeleccionada[i] === usuarioLetra) {
+                    usuarioAdivina[i] = usuarioLetra;
+                }
+            }
+        }   
+
     }
 
+    console.log("Letras adivinadas:", usuarioAdivina);
 
-} while (!usuarioAdivino);
-const letraUsuario = prompt
+//} while (!usuarioAdivino);
